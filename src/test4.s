cfa39f2e74497b678ce8feafd1bda4604082ab1e
0918nobita <nobita.0918@gmail.com>
2019-02-09 13:01:13 +0900

    :sparkles: 関数呼び出しに関するC言語/アセンブリプログラムを追加した

---
+++ src/test4.s
@@ -0,0 +1,12 @@
+.intel_syntax noprefix
+.global plus, main
+
+plus:
+        add rsi, rdi
+        mov rax, rsi
+        ret
+main:
+        mov rdi, 3
+        mov rsi, 4
+        call plus
+        ret
