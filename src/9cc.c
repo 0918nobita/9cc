ab48be2a66bbb8bf7030259e7e0928f0920e2086
0918nobita <nobita.0918@gmail.com>
2019-02-10 17:01:21 +0900

    :sparkles: C言語で実装しているコンパイラにトークナイザを追加して、空白文字を含む加減算の式でもコンパイルできるようにした

--- src/9cc.c
+++ src/9cc.c
@@ -1,5 +1,59 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty;
+  int val;
+  char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "トークナイズできません: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+// エラーを報告するための関数
+void error(int i) {
+  fprintf(stderr, "予期しないトークンです: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,26 +61,34 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // トークナイズする
+  tokenize(argv[1]);
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  char *p = argv[1];
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  // 式の最初は数でなければならないので、それをチェックして最初の mov 命令を出力
+  if (tokens[0].ty != TK_NUM) error(0);
+  printf("  mov rax, %d\n", tokens[0].val);
 
-  while (*p) {
-    switch (*p) {
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    switch (tokens[i].ty) {
       case '+':
-        p++;
-        printf("  add rax, %ld\n", strtol(p, &p, 10));
+        i++;
+        if (tokens[i].ty != TK_NUM) error(i);
+        printf("  add rax, %d\n", tokens[i].val);
+        i++;
         continue;
       case '-':
-        p++;
-        printf("  sub rax, %ld\n", strtol(p, &p, 10));
+        i++;
+        if (tokens[i].ty != TK_NUM) error(i);
+        printf("  sub rax, %d\n", tokens[i].val);
+        i++;
         continue;
       default:
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-        return 1;
+        error(i);
     }
   }
 
